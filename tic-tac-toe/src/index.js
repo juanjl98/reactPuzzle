963ea53e64af0c44b06d550ce9da6fcefc172428
juanjl98 <juanjimenezleon98@uma.es>
2022-05-07 17:29:42 +0200

    MVP realizado

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,6 +1,10 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Image from "./assets/title.png"
+import Equis from "./assets/equis.png"
+import Circulo from "./assets/circulo.png"
+import Empty from "./assets/empty.png"
 
 
 
@@ -13,32 +17,64 @@ declarar el objeto.
 
 También podemos usar CSS con la propiedad className
 
-
 */
 
 
 const Square = (props) => {
+
+  const [value, setValue] = useState(null)
+  //Llamando a setValue actualizamos el estado de value y actualizamos el componente.
   return (
-    <button className="square" onClick={() => alert(`square ${props.value} clicked`)}>
-      {props.value}
+    <div>
+    <button style={{background: `url(${props.value == 'X' ? Equis : props.value == 'O' ? Circulo : Empty})`}}
+      className="square" onClick={props.onClickEvent}> 
     </button>
+    </div>
   )
 }
 
 const Board = () => {
 
+  /*
+    Al pasar el estado de cada cuadrado al componente Board, hacemos lo que se conoce
+    como "lifting state up".
+  */
+  const initialSquares = Array(9).fill(null)
+  const [squares, setSquares] = useState(initialSquares)
+  const [xIsNext, setXIsNext] = useState(true)
+
+  const handleClickEvent = (i) => {
+    //Para cambiar el estado, tomaremos una copia de initialSquares
+    if (calculateWinner(squares) != null) {
+      setSquares(initialSquares)
+      setXIsNext(true)
+    } else {
+      if (squares[i] == null) {
+        const newSquares = [...squares]
+        //La actualizaremos ocn el valor cambiado
+        newSquares[i] = xIsNext ? 'X' : 'O';
+        //Y llamaremos a la función setSquares, que actualizará los componentes
+        setSquares(newSquares)
+        setXIsNext(!xIsNext)
+      }
+    }
+    
+    
+  }
+
+  const winner = calculateWinner(squares)
+  const turno = winner == null ? `Turno de ${xIsNext ? 'X' : 'O'}` : winner == 'X' ? `X Ganó` : 'O Ganó'
+
   const renderSquare = (i) => {
     return ( 
-      <Square value={i}/> //Así se pasan los props.
-      )
+      <Square value={squares[i]}                          //Así se pasan los props.
+              onClickEvent={() => handleClickEvent(i)}/>  //Pasamos la función como prop. La función accederá al estado de Board
+      )                                                   //y el componente y sus hijos se actualizarán
   }
+
   return (
-    <div style={{
-      backgroundColor: 'skyblue',
-      margin: 10,
-      padding: 20
-    }}>
-      Board
+    <div className="tablero">
+      <div className="turno">{turno}</div>
       <div className = "board-row">
         {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
       </div>
@@ -55,8 +91,9 @@ const Game = () => {
 
   return (
     <div className="game">
-      Game
+      <img className="imageHeader" src={Image}></img>
       <Board/>
+      
     </div>
   )
 }
@@ -71,3 +108,21 @@ ReactDOM.render(
   <Game />, 
   document.getElementById('root')
 )
+
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+  ]
+
+  for (let line of lines) {
+    const [a,b,c] = line
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+
+  return null
+}
